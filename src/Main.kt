4e09926cefeba3b859bbe07127f2456df517a724
Angichaika <124779069+Angichaika@users.noreply.github.com>
2023-04-27 11:27:18 +0200

    commit init

---
+++ src/Main.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    for (i in 1..100 step 2) {
+        when (i) {
+            in 1..50 -> above50(i)
+            in 50..100 -> below50(i)
+        }
+    }
+
+
+}
+
+fun above50(i: Int) {
+    println("numeri sotto 50: $i")
+}
+fun below50(i: Int){
+    println("numeri sopra 50: $i")
+}
+
